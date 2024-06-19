ab1c14b54ed3f22d4951c92f8a4397a0946a5f57
David Fabian <usernamedf@outlook.com>
2024-06-19 18:27:50 -0400

    moved code in resize.py to create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -2,12 +2,43 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from PIL import Image
-from resize import ImgResize, PadToSquare
-import numpy
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-BATCH_SIZE = 32
+class ImgResize(object):
+    def __call__(self, img: Image, output_size=(256,256)):
+        image = img
+
+        original_width, original_height = image.size
+        aspect_ratio = original_width / original_height
+
+        target_width, target_height = output_size
+        if target_width / target_height > aspect_ratio:
+            target_width = int(target_height * aspect_ratio)
+        else:
+            target_height = int(target_width / aspect_ratio)
+
+        # Resize the image
+        resized_image = image.resize((target_width, target_height), Image.LANCZOS)
+
+        return resized_image
+    
+class PadToSquare:
+    def __call__(self, img: Image):
+
+        width, height = img.size
+ 
+        target_size = max(width, height)
+
+        new_img = Image.new("RGB", (target_size, target_size), (0, 0, 0))
+  
+        x_offset = (target_size - width) // 2
+        y_offset = (target_size - height) // 2
+        
+        new_img.paste(img, (x_offset, y_offset))
+        
+        return new_img
+
+BATCH_SIZE = 64
 
 transform = transforms.Compose([
     ImgResize(),
@@ -16,6 +47,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.29179502, 0.27263689, 0.27018078], 
                          std=[0.32638136, 0.30372527, 0.31044443])
     ])
+
 
 dataset = ImageFolder("ImageData", transform=transform)
 train_set, test_set = train_test_split(dataset, test_size=0.2, random_state=0)
